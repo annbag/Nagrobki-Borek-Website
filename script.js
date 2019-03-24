9e7478d14b422060aa4341227a25d9236353862b
Anna Bagrowska <anna_bagrowska@wp.pl>
2019-03-24 23:37:51 +0100

    add map

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ hamburger.addEventListener('click', function () {
     menu.classList.toggle('active');
 })
 
+//add map
+function initMap() {
+    const place = {
+        lat: 52.719531,
+        lng: 23.504038
+    };
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 13,
+            center: place
+        });
+    var marker = new google.maps.Marker({
+        position: place,
+        map: map
+    });
+}
