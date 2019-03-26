fa3041b833e009d859618983a874d28cfea27403
Anna Bagrowska <anna_bagrowska@wp.pl>
2019-03-26 23:04:32 +0100

    add hide nav

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 var hamburger = document.querySelector('.hamburger');
 var menu = document.querySelector('.menu');
+var navbar = document.getElementById('navbar')
 
-hamburger.addEventListener('click', function () {
-    this.classList.toggle('active');
-    menu.classList.toggle('active');
-})
+hamburger.addEventListener('click', function() {
+  this.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+//hide nav
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = '0';
+  } else {
+    navbar.style.top = '-70px';
+  }
+  prevScrollpos = currentScrollPos;
+};
 
 //add map
 function initMap() {
-    const place = {
-        lat: 52.719531,
-        lng: 23.504038
-    };
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 13,
-            center: place
-        });
-    var marker = new google.maps.Marker({
-        position: place,
-        map: map
-    });
+  const place = {
+    lat: 52.719531,
+    lng: 23.504038
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: place
+  });
+  var marker = new google.maps.Marker({
+    position: place,
+    map: map
+  });
 }
